59fca31aa83792a43b647a7dd86f5e3de626afc9
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2022-12-06 01:30:54 -0600

    Add condition for registration page

--- users/views.py
+++ users/views.py
@@ -17,7 +17,7 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    return render(request, 'users/register.html', {'form': form, 'registration':True})
 
 
 @login_required
